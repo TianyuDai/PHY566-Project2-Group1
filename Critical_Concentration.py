a5acf17ae05996ed9cecb19be45fa940ae847ca2
TianyuDai <td115@duke.edu>
2017-04-03 23:25:36 -0400

    Update Critical_Concentration.py

--- Critical_Concentration.py
+++ Critical_Concentration.py
@@ -1,56 +1,53 @@
 import pylab as pl  
-import random 
-import numpy as np
+import random
 import time
 time1 = time.time()
 
-N = [5,10,15,20,30,50,80] # the length of sides of a grid
-simulations = 50
-Pc = [0.0]*len(N)
+N = [5,10,15,20,] # the length of sides of a grid
+Pc = []
 
-for n in range(len(N)):
-    print "grid length", N[n]
-    for m in range(simulations):
-        print m
-        grid = pl.zeros([N[n]+2,N[n]+2]) # generate an empty grid
-        grid[1:(N[n]+1),0] = pl.linspace(1,N[n],N[n]) # boundary condition
-        grid[0,1:(N[n]+1)] = pl.linspace(N[n]+1,2*N[n],N[n])
-        grid[1:(N[n]+1),N[n]+1] = pl.linspace(2*N[n]+1,3*N[n],N[n])
-        grid[N[n]+1,1:(N[n]+1)] = pl.linspace(3*N[n]+1,4*N[n],N[n])
+for n in N:
+    Pc_n = 0
+    for m in range(50):
+        grid = pl.zeros([n+2,n+2]) # generate an empty grid
+        # boundary condition
+        grid[1:(n+1),0] = pl.linspace(1,n,n)
+        grid[0,1:(n+1)] = pl.linspace(n+1,2*n,n)
+        grid[1:(n+1),n+1] = pl.linspace(2*n+1,3*n,n)
+        grid[n+1,1:(n+1)] = pl.linspace(3*n+1,4*n,n)
+        s = [0]*(n)+[n+1]*(n)+range(1,n+1)+range(1,n+1) # x coordinate of percolated sites
+        t = range(1,n+1)+range(1,n+1)+[0]*(n)+[n+1]*(n) # y coordinate of percolated sites
         Check = [1, 0, 0, 0]
-        clusnum = 4*N[n]
-        while np.any(Check):
-            Ranm = random.randint(0,N[n]**2-1) # generate a random number
-            x = Ranm/N[n]+1 # xth-row
-            y = Ranm%N[n]+1# yth-column
+        clusnum = 4*n
+        while pl.any(Check):
+            Ranm = random.randint(0,n**2-1) # generate a random number
+            x = Ranm/n+1 # xth-row
+            y = Ranm%n+1# yth-column
+            s.append(x)
+            t.append(y)
             if grid[x,y] == 0: # check whether the site (x,y) is empty
                 clusnum += 1
                 grid[x,y] = clusnum # put a point at site (x,y)
                 Neighbor = [grid[x-1,y],grid[x+1,y],grid[x,y-1],grid[x,y+1]]
                 # check neighbering sites
                 if max(grid[x-1,y],grid[x+1,y],grid[x,y-1],grid[x,y+1]):
-                    for i in range(N[n]+2): 
-                        for j in range(N[n]+2): 
-                            if grid[i,j]:
-                                if np.any(Neighbor==grid[i,j]):
-                                    grid[i,j] = clusnum
-            Check = [clusnum-max(grid[0,:]),clusnum-max(grid[N[n]+1,:]),clusnum-max(grid[:,0]),clusnum-max(grid[:,N[n]+1])]
-        Pc[n] += (clusnum-4*N[n])/float(N[n]**2*simulations)
+                    for (i,j) in zip(s,t): 
+                        if pl.any(Neighbor==grid[i,j]):
+                            grid[i,j] = clusnum
+            Check = [clusnum-max(grid[0,:]),clusnum-max(grid[n+1,:]),clusnum-max(grid[:,0]),clusnum-max(grid[:,n+1])]
+        Pc_n += n**2+4-sum(sum(grid[i,:]==0) for i in range(n+2))
+    Pc.append(Pc_n/float(n**2*50))
 
 #save
 np.savetxt('pc.txt',Pc)
 #Pc=np.loadtxt('pc.txt')
         
 #plot
-inverseN = [1.0/k for k in N]
-pl.plot(inverseN,Pc,'o-b')
-pl.title('Critical Concentration') 
+time2 = time.time()
+inverseN = [1.0/5,1.0/10,1.0/15]
+pl.plot(inverseN,Pc,'-bo')
+pl.title('Critical Concentration\nCalculation Time = %.2fs' % (time2-time1)) 
 pl.xlabel('$1/N$') 
 pl.ylabel('Pc') 
-pl.savefig('Critical_2.pdf') 
-pl.show()  
-
-time2 = time.time()
-print time2 - time1
-
-
+pl.savefig('Critical.pdf')
+pl.show() 
