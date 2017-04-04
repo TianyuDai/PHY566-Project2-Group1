74e31219fe374a336899ba6e2299596cff188955
TianyuDai <td115@duke.edu>
2017-04-04 14:04:29 -0400

    Update Fraction.py

--- Fraction.py
+++ Fraction.py
@@ -1,6 +1,5 @@
 import pylab as pl  
-import random 
-import numpy as np
+import random
 import math
 
 N = 50 # the length of sides of a grid
@@ -10,54 +9,45 @@ Fraction = [0.0]*len(P) # a list to record different fractions
 Pc = 0.6
 
 # loop
-for n in range(len(P)):
-    print "P =", P[n]
+for p in P:
     for m in range(simulations):
-        print m
         grid = pl.zeros([N+2,N+2]) # generate an empty grid
         grid[1:(N+1),0] = pl.linspace(1,N,N) # boundary condition
         grid[0,1:(N+1)] = pl.linspace(N+1,2*N,N)
         grid[1:(N+1),N+1] = pl.linspace(2*N+1,3*N,N)
         grid[N+1,1:(N+1)] = pl.linspace(3*N+1,4*N,N)
+        s = [0]*(N)+[N+1]*(N)+range(1,N+1)+range(1,N+1) # x coordinate of percolated sites
+        t = range(1,N+1)+range(1,N+1)+[0]*(N)+[N+1]*(N) # y coordinate of percolated sites
         judge = 0
         clusnum = 4*N
-        while (clusnum-4*N) < P[n]*N**2:
+        while (clusnum-4*N) < p*N**2:
             Ranm = random.randint(0,N**2-1) # generate a random number
             x = Ranm/N+1 # xth-row
             y = Ranm%N+1# yth-column
-            if grid[x,y] == 0: # check whether the site (x,y) is empty
+            if not grid[x,y]: # check whether the site (x,y) is empty
                 clusnum += 1
                 grid[x,y] = clusnum # put a point at site (x,y)
                 Neighbor = [grid[x-1,y],grid[x+1,y],grid[x,y-1],grid[x,y+1]]
                 if max(grid[x-1,y],grid[x+1,y],grid[x,y-1],grid[x,y+1]): # check neighbering sites
-                     for i in range(N+2): 
-                        for j in range(N+2):
-                            if grid[i,j]:
-                                if np.any(Neighbor==grid[i,j]):
-                                    grid[i,j] = clusnum
+                     for (i,j) in zip(s,t):
+                        if pl.any(Neighbor==grid[i,j]):
+                            grid[i,j] = clusnum
+                s.append(x)
+                t.append(y)
             Check = [clusnum-max(grid[0,:]),clusnum-max(grid[N+1,:]),clusnum-max(grid[:,0]),clusnum-max(grid[:,N+1])]   
-            if not np.any(Check):
+            if not pl.any(Check):
                 judge = clusnum
                 
-        counter = 0
-        for i in range(1,N+1):
-            for j in range(1,N+1):
-                if grid[i,j] == judge:
-                    counter += 1 # number of sites in spanning cluster            
-        Fraction[n] += counter/float(N**2*P[n]*simulations)
- 
+        counter = sum(sum(grid[i,:]==judge) for i in range(1,N+1)) # number of sites in spanning cluster         
+        Fraction.append(counter/float(N**2*p*simulations))
+
 deltaP = [Pn-Pc for Pn in P]
+
+pl.savetxt('Fraction_1.txt',Fraction)
+
 pl.plot([math.log(i) for i in deltaP],[math.log(j) for j in Fraction],'o-b')         
 pl.title('Fraction N=%d' %N) 
 pl.xlabel('$log(P-Pc)$') 
 pl.ylabel('$log(F)$') 
 pl.savefig('fraction.pdf') 
-pl.show()  
-
-np.savetxt('Fraction_1.txt',Fraction)
-   
-
-
-
-
-
+pl.show()
